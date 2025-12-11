fdb0d74fd687f851c729f1aebe2b1c863f42620c
fahimmuntacher <muntacherfahim234@gmail.com>
2025-12-11 15:58:45 +0600

    payment session create api created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const stripe = require("stripe")(`${process.env.STRIPE_KEY}`);
+
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
@@ -9,13 +11,12 @@ const port = 3000;
 var serviceAccount = require("./firebase_admin_sdk_key.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 // middlewear
 app.use(express.json());
 app.use(cors());
-
 
 // jwt verification middlewear
 const verifyFireBaseToke = async (req, res, next) => {
@@ -51,59 +52,84 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const myDb = client.db("soreli_db"); 
+    const myDb = client.db("soreli_db");
     const usersCollection = myDb.collection("users");
     const lessonsCollection = myDb.collection("lessons");
 
-    
-    // users post 
-    app.post("/users", async(req, res) => {
-        const usersDetail = req.body;
-        usersDetail.role = "user";
-        usersDetail.isPremium = false;
-        usersDetail.createdAt = new Date();
-        const email = usersDetail?.email;
-        const existingUser = await usersCollection.findOne({email});
-        if(existingUser){
-            return res.send({message : "User exist"})
-        }
-        const result = await usersCollection.insertOne(usersDetail);
-        res.send(result)
-    })
+    // users post
+    app.post("/users", async (req, res) => {
+      const usersDetail = req.body;
+      usersDetail.role = "user";
+      usersDetail.isPremium = false;
+      usersDetail.createdAt = new Date();
+      const email = usersDetail?.email;
+      const existingUser = await usersCollection.findOne({ email });
+      if (existingUser) {
+        return res.send({ message: "User exist" });
+      }
+      const result = await usersCollection.insertOne(usersDetail);
+      res.send(result);
+    });
 
-    // users get 
-    app.get("/users/:email/role", async(req, res) => {
-        const email = req.params.email;
-        const query = {email};
-        const user = await usersCollection.findOne(query);
-        res.send({role : user?.role , isPremium : user?.isPremium});
-    })
-
-
+    // users get
+    app.get("/users/:email/role", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ role: user?.role, isPremium: user?.isPremium });
+    });
 
     // lessons API
 
     // lessons post
     app.post("/lessons", verifyFireBaseToke, async (req, res) => {
       const lessonsDetail = req.body;
-      console.log(lessonsDetail);
+      // console.log(lessonsDetail);
       lessonsDetail.createdAt = new Date();
+      lessonsDetail.isFeatured = false;
       const email = lessonsDetail?.authorEmail;
-      const userRole = await usersCollection.findOne({email});
-      if(userRole?.role !== "admin"){
-        return res.status(403).send({message : "forbidden access"})
+      const userRole = await usersCollection.findOne({ email });
+      if (userRole?.role !== "user") {
+        return res.status(403).send({ message: "forbidden access" });
       }
       const result = await lessonsCollection.insertOne(lessonsDetail);
       res.send(result);
-    })
+    });
 
+    // stripe payment integration
+    app.post(
+      "/create-checkout-session",
+      verifyFireBaseToke,
+      async (req, res) => {
+        try {
+          const { price } = req.body;
+          const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: [
+              {
+                price_data: {
+                  currency: "bdt",
+                  unit_amount: Number(price) * 100, // 1500 BDT → 150000
+                  product_data: {
+                    name: "Premium Membership - Lifetime Access",
+                  },
+                },
+                quantity: 1,
+              },
+            ],
+            userEmail : req.decoded_email,
+            mode: "payment",
+            success_url: `${process.env.DOMAIN_NAME}/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${process.env.DOMAIN_NAME}/checkout/cancel`,
+          });
 
-
-
-
-
-
-
+          res.json({ url: session.url }); // frontend will redirect
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: "Stripe session creation failed" });
+        }
+      }
+    );
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
